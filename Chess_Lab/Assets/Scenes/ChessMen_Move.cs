54983969d10e6358bd67bf90db7faf9b9f9ed263
david-wei0628 <62085333+david-wei0628@users.noreply.github.com>
2021-11-17 12:12:10 +0800

    移動方法修改
    對稱的移動規則 判斷改用絕對值 減少IF
    但士兵的黑白棋移動未調整 黑棋移動方向錯誤
    棋子移動方法改至其他CS內
    Collider變化還未併入移動CS
    變數暫時調整共用

--- Chess_Lab/Assets/Scenes/ChessMen_Move.cs
+++ Chess_Lab/Assets/Scenes/ChessMen_Move.cs
@@ -5,6 +5,59 @@ namespace Chess.ChessMen
 {
     public class ChessMen_Move : MonoBehaviour
     {
-        
+        public GameObject Select_Move(GameObject ChessMen,Vector3 maps,string Lock_objext)
+        {
+
+            switch (ChessMen.tag)
+            {
+                case "Pawn":
+                    if (ChessMen.transform.position.z == 100 && ChessMen.transform.position.x == maps.x)
+                    {
+                        if (maps.z - ChessMen.transform.position.z == 200 || maps.z - ChessMen.transform.position.z == 100)
+                            ChessMen.transform.position = Vector3.Lerp(ChessMen.transform.position, maps, 1.0f);
+                    }
+                    if (maps.z - ChessMen.transform.position.z == 100 && maps.z <= 700 && ChessMen.transform.position.x == maps.x)
+                        ChessMen.transform.position = Vector3.Lerp(ChessMen.transform.position, maps, 1.0f);
+                    break;
+                case "Knight":
+                    if (Mathf.Abs(maps.z - ChessMen.transform.position.z) == 200 && maps.z <= 700 && maps.z >= 0)
+                        if (Mathf.Abs(maps.x - ChessMen.transform.position.x) == 100)
+                            ChessMen.transform.position = Vector3.Lerp(ChessMen.transform.position, maps, 1f);
+                    if (Mathf.Abs(maps.z - ChessMen.transform.position.z) == 100 && maps.z <= 700 && maps.z >= 0)
+                        if (Mathf.Abs(maps.x - ChessMen.transform.position.x) == 200)
+                            ChessMen.transform.position = Vector3.Lerp(ChessMen.transform.position, maps, 1f);
+                    break;
+                case "Rook":
+                    if (maps.z == ChessMen.transform.position.z && maps.x <= 700)
+                        ChessMen.transform.position = Vector3.Lerp(ChessMen.transform.position, maps, 1f);
+                    else if (maps.x == ChessMen.transform.position.x && maps.z <= 700)
+                        ChessMen.transform.position = Vector3.Lerp(ChessMen.transform.position, maps, 1f);
+                    break;
+                case "bishop":
+                    if (Mathf.Abs(maps.z - ChessMen.transform.position.z) == Mathf.Abs(maps.x - ChessMen.transform.position.x) && maps.z <= 700 && maps.x <= 700)
+                        ChessMen.transform.position = Vector3.Lerp(ChessMen.transform.position, maps, 1f);
+                    break;
+                case "Queen":
+                    if (maps.z == ChessMen.transform.position.z && maps.x <= 700)
+                        ChessMen.transform.position = Vector3.Lerp(ChessMen.transform.position, maps, 1f);
+                    if (maps.x == ChessMen.transform.position.x && maps.z <= 700)
+                        ChessMen.transform.position = Vector3.Lerp(ChessMen.transform.position, maps, 1f);
+                    if (Mathf.Abs(maps.z - ChessMen.transform.position.z) == Mathf.Abs(maps.x - ChessMen.transform.position.x) && maps.z <= 700 && maps.x <= 700)
+                        ChessMen.transform.position = Vector3.Lerp(ChessMen.transform.position, maps, 1f);
+                    break;
+                case "King":
+                    if (Mathf.Abs(maps.z - ChessMen.transform.position.z) == 100 && maps.z <= 700)
+                        ChessMen.transform.position = Vector3.Lerp(ChessMen.transform.position, maps, 1f);
+                    if (Mathf.Abs(maps.x - ChessMen.transform.position.x) == 100 && maps.x <= 700)
+                        ChessMen.transform.position = Vector3.Lerp(ChessMen.transform.position, maps, 1f);
+                    break;
+            }
+            if (Lock_objext != "Board" && ChessMen.transform.position.y > 0)
+                ChessMen.transform.position = new Vector3(ChessMen.transform.position.x, ChessMen.transform.position.y - 50, ChessMen.transform.position.z);
+
+            return ChessMen;
+        }
+
     }
+    
 }
--- Chess_Lab/Assets/Scenes/Chess_Move.cs
+++ Chess_Lab/Assets/Scenes/Chess_Move.cs
@@ -1,7 +1,7 @@
-﻿using Chess.ChessMen;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Chess.ChessMen;
 
 public class Chess_Move : MonoBehaviour
 {
@@ -12,19 +12,28 @@ public class Chess_Move : MonoBehaviour
     }
 
     public Camera main_camear;
-    GameObject ChessMen;
-    string Lock_objext;
-    Vector3 maps;
-    Ray ray;
-    RaycastHit hit;
+    public static GameObject ChessMen;
+    public static string Lock_objext;
+    public static Vector3 maps;
+    public static Ray ray;
+    public static RaycastHit hit;
+    ChessMen_Move ChessMen_Move = new ChessMen_Move();
+    
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0) && Lock_objext == "Board")
             Select_Chess();
         else if (Input.GetMouseButtonDown(0) && Lock_objext != "Board")
-            Select_Move();
+        {
+            Select_Ray();
+            SetChessColliderEnabled();
+            ChessMen = ChessMen_Move.Select_Move(ChessMen,maps,Lock_objext);
+            Lock_objext = "Board";
+            //Select_Move();
+        }
     }
+
     /// <summary>
     /// 基礎完成後，此方法轉到ChessMen_Move.cs使用，因為有雙色棋用同方法
     /// </summary>
@@ -58,17 +67,11 @@ public class Chess_Move : MonoBehaviour
                     ChessMen.transform.position = Vector3.Lerp(ChessMen.transform.position, maps, 1.0f);
                 break;
             case "Knight":
-                if (maps.z - ChessMen.transform.position.z == 200 && maps.z <= 700)
-                    if (maps.x - ChessMen.transform.position.x == 100 || maps.x - ChessMen.transform.position.x == -100)
+                if (Mathf.Abs(maps.z - ChessMen.transform.position.z) == 200 && maps.z <= 700 && maps.z >= 0)
+                    if (Mathf.Abs(maps.x - ChessMen.transform.position.x) == 100)
                         ChessMen.transform.position = Vector3.Lerp(ChessMen.transform.position, maps, 1f);
-                if (maps.z - ChessMen.transform.position.z == -200 && maps.z <= 700)
-                    if (maps.x - ChessMen.transform.position.x == 100 || maps.x - ChessMen.transform.position.x == -100)
-                        ChessMen.transform.position = Vector3.Lerp(ChessMen.transform.position, maps, 1f);
-                if (maps.z - ChessMen.transform.position.z == 100 && maps.z <= 700)
-                    if (maps.x - ChessMen.transform.position.x == 200 || maps.x - ChessMen.transform.position.x == -200)
-                        ChessMen.transform.position = Vector3.Lerp(ChessMen.transform.position, maps, 1f);
-                if (maps.z - ChessMen.transform.position.z == -100 && maps.z <= 700)
-                    if (maps.x - ChessMen.transform.position.x == 200 || maps.x - ChessMen.transform.position.x == -200)
+                if (Mathf.Abs(maps.z - ChessMen.transform.position.z) == 100 && maps.z <= 700 && maps.z >= 0)
+                    if (Mathf.Abs(maps.x - ChessMen.transform.position.x) == 200)
                         ChessMen.transform.position = Vector3.Lerp(ChessMen.transform.position, maps, 1f);
                 break;
             case "Rook":
@@ -78,9 +81,7 @@ public class Chess_Move : MonoBehaviour
                     ChessMen.transform.position = Vector3.Lerp(ChessMen.transform.position, maps, 1f);
                 break;
             case "bishop":
-                if (maps.z - ChessMen.transform.position.z == maps.x - ChessMen.transform.position.x && maps.z <= 700 && maps.x <= 700)
-                    ChessMen.transform.position = Vector3.Lerp(ChessMen.transform.position, maps, 1f);
-                if (maps.z - ChessMen.transform.position.z == ChessMen.transform.position.x - maps.x && maps.z <= 700 && maps.x <= 700)
+                if (Mathf.Abs(maps.z - ChessMen.transform.position.z) == Mathf.Abs(maps.x - ChessMen.transform.position.x) && maps.z <= 700 && maps.x <= 700)
                     ChessMen.transform.position = Vector3.Lerp(ChessMen.transform.position, maps, 1f);
                 break;
             case "Queen":
@@ -88,19 +89,13 @@ public class Chess_Move : MonoBehaviour
                     ChessMen.transform.position = Vector3.Lerp(ChessMen.transform.position, maps, 1f);
                 if (maps.x == ChessMen.transform.position.x && maps.z <= 700)
                     ChessMen.transform.position = Vector3.Lerp(ChessMen.transform.position, maps, 1f);
-                if (maps.z - ChessMen.transform.position.z == maps.x - ChessMen.transform.position.x && maps.z <= 700 && maps.x <= 700)
-                    ChessMen.transform.position = Vector3.Lerp(ChessMen.transform.position, maps, 1f);
-                if (maps.z - ChessMen.transform.position.z == ChessMen.transform.position.x - maps.x && maps.z <= 700 && maps.x <= 700)
+                if (Mathf.Abs(maps.z - ChessMen.transform.position.z) == Mathf.Abs(maps.x - ChessMen.transform.position.x) && maps.z <= 700 && maps.x <= 700)
                     ChessMen.transform.position = Vector3.Lerp(ChessMen.transform.position, maps, 1f);
                 break;
             case "King":
-                if (maps.z - ChessMen.transform.position.z == 100 && maps.z <= 700)
+                if (Mathf.Abs(maps.z - ChessMen.transform.position.z) == 100 && maps.z <= 700)
                     ChessMen.transform.position = Vector3.Lerp(ChessMen.transform.position, maps, 1f);
-                if (maps.z - ChessMen.transform.position.z == -100 && maps.z <= 700)
-                    ChessMen.transform.position = Vector3.Lerp(ChessMen.transform.position, maps, 1f);
-                if (maps.x - ChessMen.transform.position.x == 100 && maps.x <= 700)
-                    ChessMen.transform.position = Vector3.Lerp(ChessMen.transform.position, maps, 1f);
-                if (maps.x - ChessMen.transform.position.x == -100 && maps.x <= 700)
+                if (Mathf.Abs(maps.x - ChessMen.transform.position.x) == 100 && maps.x <= 700)
                     ChessMen.transform.position = Vector3.Lerp(ChessMen.transform.position, maps, 1f);
                 break;
         }
@@ -137,4 +132,5 @@ public class Chess_Move : MonoBehaviour
         for (int i = 0; i < this.GetComponentsInChildren<Collider>().Length; i++)
             this.GetComponentsInChildren<Collider>()[i].enabled = !this.GetComponentsInChildren<Collider>()[i].enabled;
     }
+    
 }
